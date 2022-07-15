37c5ccb15545002f4326eef063eafa8a5c1658c5
Sotsil Silva <ada_hack127@hackademy.mx>
2022-07-15 13:45:00 -0500

    configurado el settings, agregados ambientes dev y prod

--- lugares_seguros2/lugares_seguros2/settings/base.py
+++ lugares_seguros2/lugares_seguros2/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cws#+(!l!1brup0(w_xecg1z8n+mo2@v+oma6x=j90kq&d1(0b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-Mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
