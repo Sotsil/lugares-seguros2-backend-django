b5d2297b21095d37fa9e9e45cbddc6b83ac31a9a
Sotsil Silva <ada_hack127@hackademy.mx>
2022-07-19 18:09:42 -0500

    installed DRF
    added hello world

--- lugares_seguros2/lugares_seguros2/settings/base.py
+++ lugares_seguros2/lugares_seguros2/settings/base.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd apps
+    'rest_framework',
+    #local apps
+    'initial',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
